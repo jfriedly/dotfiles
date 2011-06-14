22375ac0343f6e14873414ac4df4365c2a708a70
Joel Friedly <joelfriedly@gmail.com>
2011-06-14 06:07:34 -0400

    Setup my git with two commands

---
+++ git-setup.sh
@@ -0,0 +1,4 @@
+git config --global user.name "Joel Friedly"
+git config --global user.email "joelfriedly@gmail.com"
+git config --global github.user jfriedly
+echo "Don't forget to add your API token with 'git config --global github.token XXXXX...'"
