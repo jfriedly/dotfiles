ed0a493b29d2860c9cc91de871b44a015a8e3650
Joel Friedly <joelfriedly@gmail.com>
2012-07-09 14:48:53 -0700

    Added ssh-config

--- git-setup.sh
+++ git-setup.sh
@@ -3,4 +3,6 @@ git config --global user.email "joelfriedly@gmail.com"
 git config --global github.user jfriedly
 git config --global core.editor vim
 git config --global color.ui always
+echo "Copying ssh config over"
+cp ssh-config ~/.ssh/config
 echo "Don't forget to add your API token with 'git config --global github.token XXXXX...'"
