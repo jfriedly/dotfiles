8c43151174db175176b7399a56bdfb62a71d22a6
Joel Friedly <joelfriedly@gmail.com>
2012-11-19 10:38:03 -0500

    Added lots of ssh stuff, moved over to one script to set everything up.

--- git-setup.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.name "Joel Friedly"
-git config --global user.email "joelfriedly@gmail.com"
-git config --global github.user jfriedly
-git config --global core.editor vim
-git config --global color.ui always
-echo "Copying ssh config over"
-cp ssh-config ~/.ssh/config
---
+++ setup.sh
@@ -0,0 +1,18 @@
+echo "Configuring git"
+git config --global user.name "Joel Friedly"
+git config --global user.email "joelfriedly@gmail.com"
+git config --global github.user jfriedly
+git config --global core.editor vim
+git config --global color.ui always
+echo "Copying over .vimrc"
+cp .vimrc $HOME/.
+echo "Copying over .bashrc"
+cp .bashrc $HOME/.
+echo "Configuring ssh, don't forget to copy over id_rsa"
+if [ ! -e $HOME/.ssh ]
+then
+    mkdir $HOME/.ssh
+fi
+cp ssh-config $HOME/.ssh/config
+cp authorized_keys $HOME/.ssh/.
+cp id_rsa.pub $HOME/.ssh/.
