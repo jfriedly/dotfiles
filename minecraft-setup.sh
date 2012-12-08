fbec658e716175bb82199b9e0c9b418bd139cf31
Joel Friedly <joelfriedly@gmail.com>
2012-12-08 01:14:01 +0000

    Added a minecraft bootstrap script

---
+++ minecraft-setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install openjdk-7-jre
+mkdir -p $HOME/minecraft
+cd $HOME/minecraft
+wget https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar
+echo "jfriedly" > ops.txt
+java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
