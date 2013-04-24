d9ac079269c6c20f016c21292c22de48d21bc862
Joel Friedly <joelfriedly@gmail.com>
2013-04-24 17:51:50 -0400

    Ended up just scp'ing crf-setup.sh over, but this was the final version

--- crf-setup.sh
+++ crf-setup.sh
@@ -1,13 +1,18 @@
 sudo apt-get update
-sudo apt-get install gcc g++ make unzip
+sudo apt-get install gcc g++ make unzip git
+git clone https://github.com/jfriedly/dotfiles.git
+cd dotfiles
+./setup.sh
+cd -
 wget https://crfpp.googlecode.com/files/CRF%2B%2B-0.58.tar.gz
 tar xf CRF++-0.58.tar.gz
 cd CRF++-0.58
 ./configure
-./make
-./make install
+make
+sudo make install
 cd -
 wget http://opensource.osu.edu/~jfriedly/G2P.zip
 unzip G2P.zip
 cd G2P
-echo "Change the template, open tmux, export LD_LIBRARY_PATH=/usr/local/lib and go!"
+chmod +x *.sh
+echo "Change the template, remove the file deletions, open tmux, export LD_LIBRARY_PATH=/usr/local/lib and go!"
